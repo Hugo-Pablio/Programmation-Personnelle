831ff18a8e03c18a25ebc38f4f92088bb8c942a3
Hugo <crepin.hugo30@gmail.com>
2022-09-24 11:33:11 +0200

    Ajout Cuisine nouvelle Ajout Slide Ajout nouvelle cuisine Mise en norme de la page WEB

--- visite.js
+++ visite.js
@@ -4,17 +4,16 @@ var hauteurImage;
 var zoomé;
 
 function zoom(image) {
-if (zoomé == 1){
-    image.style.width = largeurImage;
-    image.style.height = hauteurImage;
-    zoomé = 0;
-}
-else{
-    largeurImage = image.style.width;
-    hauteurImage = image.style.height;
-    image.style.width = 672;
-    image.style.height = 378;
-    zoomé = 1;
-}
-
+    if (zoomé == 1){
+        image.style.width = largeurImage;
+        image.style.height = hauteurImage;
+        zoomé = 0;
+    }
+    else{
+        largeurImage = image.style.width;
+        hauteurImage = image.style.height;
+        image.style.width = 672;
+        image.style.height = 378;
+        zoomé = 1;
+    }
 }
